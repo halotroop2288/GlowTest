96374086dfdd309eb216009350a4c0bd5f224dce
Axel Mbus Mbus <70901334+Azael-Snively@users.noreply.github.com>
2020-09-22 17:05:32 +0100

    Slime Ore added

--- src/main/java/com/halotroop/brightcraft/Blocks.java
+++ src/main/java/com/halotroop/brightcraft/Blocks.java
@@ -13,6 +13,11 @@ public class Blocks {
 			.with(MaterialAttribute.SPECULARITY, 0.01)
 			.with(MaterialAttribute.DIFFUSE_TEXTURE_ID, "stoneDiffuse")
 			.with(MaterialAttribute.EMISSIVITY, 0.0);
+	private static final Material.Generic MATERIAL_STONE = new Material.Generic()
+			.with(MaterialAttribute.DIFFUSE_COLOR, new Vector3d(1, 1, 1))
+			.with(MaterialAttribute.SPECULARITY, 0.01)
+			.with(MaterialAttribute.DIFFUSE_TEXTURE_ID, "slimeDiffuse")
+			.with(MaterialAttribute.EMISSIVITY, 0.0);
 	private static final Material.Generic MATERIAL_GRASS = new Material.Generic()
 			.with(MaterialAttribute.DIFFUSE_COLOR, new Vector3d(1, 1, 1))
 			.with(MaterialAttribute.SPECULARITY, 0.01)
@@ -30,6 +35,9 @@ public class Blocks {
 	public static final Block BLOCK_GRASS = new Block()
 			.setShape(VoxelShape.CUBE)
 			.setMaterial(MATERIAL_GRASS);
+	public static final Block BLOCK_STONE = new Block()
+			.setShape(VoxelShape.CUBE)
+			.setMaterial(SLIME_ORE);
 	public static final Block BLOCK_ORANGE = new Block()
 			.setShape(VoxelShape.CUBE)
 			.setMaterial(MATERIAL_ORANGE);
@@ -43,5 +51,7 @@ public class Blocks {
 		MasterRenderer.loadTexture("orangeDiffuse", "block_face_orange");
 		MasterRenderer.loadTexture("stoneDiffuse", "stone");
 		MasterRenderer.loadTexture("grassDiffuse", "grass");
+		MasterRenderer.loadTexture("grassDiffuse " , "sand" );
+
 	}
 }
